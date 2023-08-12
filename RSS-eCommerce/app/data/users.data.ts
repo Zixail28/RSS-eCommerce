74649f895198c6ce773acd595917bb7f513017fd
Lizaveta <lizabeth.nik@gmail.com>
2023-08-12 21:13:32 +0300

    feat: add poster plus siderBar

---
+++ RSS-eCommerce/app/data/users.data.ts
@@ -0,0 +1,16 @@
+type Category = {
+    id: number;
+    name: string;
+};
+  
+export const categories: Category[] = [
+    { id: 1, name: "Woman’s Fashion" },
+    { id: 2, name: "Men’s Fashion" },
+    { id: 3, name: "Electronics" },
+    { id: 4, name: "Home & Lifestyle" },
+    { id: 5, name: "Medicine" },
+    { id: 6, name: "Sports & Outdoor" },
+    { id: 7, name: "Baby’s & Toys" },
+    { id: 8, name: "Groceries & Pets" },
+    { id: 9, name: "Health & Beauty" },
+  ];
--- RSS-eCommerce/app/pages/Home/Home.tsx
+++ RSS-eCommerce/app/pages/Home/Home.tsx
@@ -1,7 +1,16 @@
-import { FC } from 'react'
+import { FC } from 'react';
+import Sidebar from '../../components/SiderBar/SiderBar';
+
+import styles from './Home.module.scss'
+import Poster from '../../components/Poster/Poster';
 
 const Home: FC = () => {
-  return <div>Home</div>
+  return (
+    <section className={styles.sidebar}>
+      <Sidebar />
+      <Poster />
+    </section>
+  )
 }
 
 export default Home
