3fe056f3f989efb59a75b50abebe9dd173a23476
Lizaveta <lizabeth.nik@gmail.com>
2023-08-13 15:35:18 +0300

    Add layout products

--- RSS-eCommerce/app/pages/Home/Home.tsx
+++ RSS-eCommerce/app/pages/Home/Home.tsx
@@ -1,15 +1,21 @@
 import { FC } from 'react';
-import Sidebar from '../../components/SiderBar/SiderBar';
 
 import styles from './Home.module.scss'
+import Sidebar from '../../components/SiderBar/SiderBar';
 import Poster from '../../components/Poster/Poster';
+import Products from '../../components/Product/Products';
+
+import { productsList } from '../../data/users.data';
 
 const Home: FC = () => {
   return (
+    <>
     <section className={styles.sidebar}>
       <Sidebar />
       <Poster />
     </section>
+    <Products products={productsList} title="Flash Sales" titlePeriod="Today's" />
+    </>
   )
 }
 
